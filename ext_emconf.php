5aa644d7639b5437a64e1b9b854214a087c764de
Niels Tiedt <niels.tiedt@gedankenfolger.de>
2026-09-21 16:21:35 +0200

    fix(composer): add TER-required dependency and packaging metadata
    Declare typo3/cms-core and friendsoftypo3/content-blocks as required
    dependencies, mark wapplersystems/ws-scss as suggested (only needed
    for the optional scss_bootstrap CSS mode), add the providesPackages
    entry required for Classic mode since TYPO3 14.2, and expand the
    package description.

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Gedankenfolger Event',
-    'description' => 'Events',
+    'description' => 'Event extension using Content Blocks, Site Set, Bootstrap CSS/SCSS and ICS export for TYPO3 13.',
     'category' => 'fe',
     'author' => 'Niels Tiedt, Gedankenfolger GmbH',
     'author_email' => 'niels.tiedt@gedankenfolger.de',
@@ -16,9 +16,12 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.99.99',
+            'typo3' => '13.4.0-13.99.99',
+            'content_blocks' => '1.3.0-1.99.99',
         ],
         'conflicts' => [],
-        'suggests' => [],
+        'suggests' => [
+            'ws_scss' => '13.0.0-13.99.99',
+        ],
     ],
 ];
